76538b72eff374cc0160d38e2e366fe8d3557c1e
unknown <jony.bhm@gmail.com>
2025-04-08 19:18:49 -0300

    android studio

---
+++ src/app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'inicio Sesion Prueba',
+  webDir: 'www'
+};
+
+export default config;
