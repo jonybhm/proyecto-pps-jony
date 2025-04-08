388ee51da55d3ccaed57f3f7a06aa77f480215a4
unknown <jony.bhm@gmail.com>
2025-04-08 19:51:34 -0300

    sync enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyDy7Sa7B--1WNMpTR3kncBMN4gBEs49TGQ",
+    authDomain: "tabs-decastro-apps.firebaseapp.com",
+    projectId: "tabs-decastro-apps",
+    storageBucket: "tabs-decastro-apps.appspot.com",
+    messagingSenderId: "641616927402",
+    appId: "1:641616927402:web:4edbcb0dff82ef774cced3"
+
+  }
 };
